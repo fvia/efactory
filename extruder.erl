5c03bfba02014feddd9c09da460b496bc6ee131f
Francesc Via <f@gaxan.com>
2017-03-26 22:34:33 +0100

    first commit

---
+++ extruder.erl
@@ -0,0 +1,72 @@
+-module(extruder).
+-behavior(gen_server).
+
+-export([start/1,stop/1]).
+-export([init/1,terminate/2,handle_call/3,handle_cast/2,handle_info/2,code_change/3]).
+
+-export([test_call/1,tick/1]).
+
+
+
+%%%%%%%%%%%%%%% ENV %%%%%%%%%%%%%%%%%%%%
+%%  {extruderName,Temperature,Speed}
+
+start(Env) -> 
+	{Name,_,_} = Env,	
+	gen_server:start_link({local,Name},?MODULE,Env,[]).
+
+stop(Name) ->
+	gen_server:cast(Name,stop).
+
+
+init(Env) -> 
+	{Name,_,_} = Env,	
+	io:format("Extruder ~p starting~n",[Name]),	
+	{ok,Env}.
+
+terminate(Reason,Env) ->
+	io:format("Extruder terminate, Reason: ~p  Env: ~p ~n",[Reason,Env]),
+	ok.
+
+
+handle_call(test_call,_From,LoopData) ->
+	{Name,_,_} = LoopData,
+	io:format("Extruder ~p test_call ~n",[Name]),
+	{reply,ok,LoopData};   
+handle_call(Message,From,LoopData) ->
+	io:format("Extruder handle_call, Msg: ~p  From: ~p  Data: ~p ~n",[Message,From,LoopData]),
+	Reply = "ja vorem",
+	{reply,Reply,LoopData}.   
+
+
+handle_cast(stop,Env) ->
+	io:format("Extruder handle_cast stop ,  Env: ~p   ~n",[Env]),
+	{stop,normal,Env};   
+handle_cast(test_call,State) ->
+	io:format("Extruder , State: ~p   ~n",[State]),
+	{noreply,State};  
+handle_cast(Request,State) ->
+	io:format("Extruder handle_cast, Request: ~p  State: ~p   ~n",[Request,State]),
+	{noreply, State}.   
+
+
+% This function is called by a gen_server process when a time-out occurs or 
+%  when it receives any other message than a synchronous or asynchronous request (or a system message).
+handle_info(Info, State) ->
+	io:format("Extruder handle_info ,  Info: ~p   ~n",[Info]), 
+	Reason = unexpected,
+	NewState = State,
+	{stop,Reason,NewState}.
+
+% Just to don't see the warning
+code_change(_OldVsn, State, _Extra) ->
+	io:format("Extruder  code_change:    ~n"), 
+	{ok, State}.
+
+
+test_call(Name) ->
+	gen_server:call( Name, test_call).
+
+tick(Name) -> 
+	gen_server:cast(Name,tick),
+	ok.	
---
+++ factory.erl
@@ -0,0 +1,78 @@
+-module(factory).
+-behavior(gen_server).
+
+-export([start/1,stop/0]).
+-export([init/1,terminate/2,handle_call/3,handle_cast/2,handle_info/2,code_change/3]).
+
+% tick: simulation advances one step 
+-export([tick/0,test_call/0]).
+
+% cd ("c:/users/fvia/documents/factory").
+
+
+start(Env) -> 
+	% 
+	gen_server:start_link({local,?MODULE},?MODULE,Env,[]).
+	%gen_server:start({local,?MODULE},?MODULE,Env,[]).
+
+stop() ->
+	gen_server:cast(?MODULE,stop).
+
+
+init(Env) -> 
+	io:format("Factory starting~n"),
+	extruder:start({e1,20,0}),
+    extruder:start({e2,20,0}),
+	{ok,Env}.
+
+terminate(Reason,Env) ->
+	extruder:stop(e1),
+	extruder:stop(e2),
+	io:format("Factory terminate, Reason: ~p  Env: ~p ~n",[Reason,Env]),
+	ok.
+
+
+
+handle_call(test_call,_From,LoopData) ->
+	_ = 10/0,
+	Reply = "okDoky",
+	{reply,Reply,LoopData};  
+handle_call(Message,From,LoopData) ->
+	io:format("Factory handle_call, Msg: ~p  From: ~p  Data: ~p ~n",[Message,From,LoopData]),
+	Reply = "ja vorem",
+	{reply,Reply,LoopData}.   
+
+
+handle_cast(stop,Env) ->
+	io:format("Factory handle_cast stop ,  Env: ~p   ~n",[Env]),
+	{stop,normal,Env};   
+handle_cast(tick,State) ->
+	extruder:tick(e1),
+	extruder:tick(e2),
+	io:format("Factory tick, State: ~p   ~n",[State]),
+	{noreply,State};  
+handle_cast(Request,State) ->
+	io:format("Factory handle_cast, Request: ~p  State: ~p   ~n",[Request,State]),
+	{noreply,"No se"}.   
+
+
+% This function is called by a gen_server process when a time-out occurs or 
+%  when it receives any other message than a synchronous or asynchronous request (or a system message).
+handle_info(Info, State) ->
+	io:format("Factory handle_info ,  Info: ~p   ~n",[Info]), 
+	Reason = unexpected,
+	NewState = State,
+	{stop,Reason,NewState}.
+
+% Just to don't see the warning
+code_change(_OldVsn, State, _Extra) ->
+	io:format("Factory  code_change:    ~n"), 
+	{ok, State}.
+
+
+tick() -> 
+	gen_server:cast(?MODULE,tick),
+	ok.	
+
+test_call() -> 
+	gen_server:call(?MODULE,test_call).		
